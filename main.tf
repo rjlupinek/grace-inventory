f8f14705d232a6addf7c043f48c3d5f681230f7f
Robert Lupinek <52836591+rjlupinek@users.noreply.github.com>
2019-11-19 17:22:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {
 
 locals {
   app_name       = "${var.project_name}-${var.appenv}-inventory"
-  account_id     = data.aws_caller_identity.current.account_id
+  account_id     = "${data.aws_caller_identity.current.account_id}"
   logging_bucket = var.appenv == "integration-testing" ? "grace-development-access-logs" : "${var.project_name}-${var.appenv}-access-logs"
 }
 
